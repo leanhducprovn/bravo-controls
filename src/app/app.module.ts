90e05d92e4b400196f97a86d9a040aeed9b366b5
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-09-20 09:26:19 +0700

    Cấu hình Project
    Hoàn thành :100:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Wijmo
+import { WjInputModule } from '@grapecity/wijmo.angular2.input';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, WjInputModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
