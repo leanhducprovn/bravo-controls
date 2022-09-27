2246541e2a3e404cb25e7cbbd09474073dd2ea86
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-09-27 16:16:02 +0700

    Khởi tạo BravoPictureInputBox
    Hoàn thành :100:

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BravoDialogDataButton } from './components/bravo.dialog.data.button/bravo.dialog.data.button';
 import { BravoPictureEditor } from './components/bravo.picture.editor/bravo.picture.editor';
+import { BravoPictureInputBoxComponent } from './components/bravo.picture.input.box/bravo.picture.input.box';
 import { BravoTabGridLayout } from './components/bravo.tab.grid.layout/bravo.tab.grid.layout';
 import { BravoToolbar } from './components/bravo.toolbar/bravo.toolbar';
 
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: 'bravo-tab-grid-layout',
     component: BravoTabGridLayout
+  },
+  {
+    path: 'bravo-picture-input-box',
+    component: BravoPictureInputBoxComponent
   }
 ];
 
---
+++ src/app/components/bravo.picture.input.box/bravo.picture.input.box.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bravo-picture-input-box',
+  templateUrl: './bravo.picture.input.box.html',
+  styleUrls: ['./bravo.picture.input.box.scss']
+})
+export class BravoPictureInputBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
