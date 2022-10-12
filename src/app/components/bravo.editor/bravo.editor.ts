9ba8978eb3cbfe800664a354a0c983b13e694304
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-10-12 11:31:33 +0700

    Đổ dữ liệu vào editor
    Hoàn thành :+1:

--- src/app/components/bravo.editor/bravo.editor.ts
+++ src/app/components/bravo.editor/bravo.editor.ts
@@ -1,48 +1,96 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, ElementRef, OnInit } from '@angular/core';
-import { DiffEditorModel } from 'ngx-monaco-editor';
 
 import * as wjc from '@grapecity/wijmo';
+
+interface IBravoEditor {
+	theme?: string,
+	language?: string,
+	value?: string,
+}
+
 @Component({
 	selector: 'bravo-editor',
 	templateUrl: './bravo.editor.html',
 	styleUrls: ['./bravo.editor.scss']
 })
+
 export class BravoEditor extends wjc.Control implements OnInit {
 
-	editorOptions = { theme: 'vs-dark', language: 'typescript' };
-	code: string = 'function x() {\nconsole.log("Hello world!");\n}';
-
-	options = {
-		theme: 'vs-dark'
+	private _options: IBravoEditor = {
+		theme: 'vs',
+		language: 'typescript',
+		value: 'Bravo Editor'
 	};
-	originalModel: DiffEditorModel = {
-		code: 'heLLo world!',
-		language: 'text/plain'
-	};
+	public set options(pValue: IBravoEditor) {
+		if (this._options == pValue)
+			return;
 
-	modifiedModel: DiffEditorModel = {
-		code: 'hello orlando!',
-		language: 'text/plain'
-	};
-
-	onInit(editor) {
-		let line = editor.getPosition();
-		console.log(line);
+		this._options = pValue;
+		this.invalidate();
+	}
+	public get options(): IBravoEditor {
+		return this._options;
 	}
 
-	constructor(elementRef: ElementRef) {
+	constructor(private http: HttpClient, elementRef: ElementRef) {
 		super(elementRef.nativeElement);
 	}
 
-	ngOnInit(): void {
-		this.test()
+	refresh(fullUpdate?: boolean): void {
+		super.refresh(fullUpdate);
 	}
 
-	test() {
-		console.log(this.hostElement.querySelector('.editor-container'))
-		wjc.setCss(this.hostElement.querySelector('.editor-container'), {
-			width: '100%',
-			height: '100%'
-		})
+	ngOnInit(): void {
+		this.initEditor();
 	}
+
+	private initEditor() {
+		// set options
+		const _api = './assets/data/cash-receipts.xml';
+		let _data: any;
+		this.http
+			.get(_api, {
+				headers: new HttpHeaders()
+					.set('Content-Type', 'text/xml')
+					.append('Access-Control-Allow-Methods', 'GET')
+					.append('Access-Control-Allow-Origin', '*')
+					.append(
+						'Access-Control-Allow-Headers',
+						'Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method'
+					),
+				responseType: 'text',
+			})
+			.subscribe(
+				(data) => {
+					_data = data;
+				},
+				(error) => {
+					console.log(error);
+				},
+				() => {
+					this.options = {
+						theme: 'vs',
+						language: 'xml',
+						value: _data
+					}
+				}
+			);
+
+		// set container
+		let _editorContainer = this.hostElement?.querySelector('.editor-container');
+		if (_editorContainer)
+			wjc.setCss(_editorContainer, {
+				width: '100%',
+				height: '100%'
+			})
+	}
+
+	public getPreferredSize() {
+		let _editor = this.hostElement?.querySelector('.bravo-editor');
+		if (_editor)
+			return new wjc.Size(_editor.clientWidth, _editor.clientHeight);
+	}
+
+
 }
