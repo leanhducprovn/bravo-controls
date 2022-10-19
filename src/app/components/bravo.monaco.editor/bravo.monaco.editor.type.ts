b87fe670f4e50cb4c0014553c5ae1520948185d2
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-10-19 16:58:52 +0700

    Cập nhật dữ liệu declare.json
    Hoàn thành :100:

--- src/app/components/bravo.monaco.editor/bravo.monaco.editor.type.ts
+++ src/app/components/bravo.monaco.editor/bravo.monaco.editor.type.ts
@@ -1,11 +1,18 @@
 import * as monaco from 'monaco-editor';
 
+// base
 export const BRAVO_MONACO_PATH = 'BRAVO_MONACO_PATH';
-
 export type BravoMonaco = typeof import('monaco-editor');
+
+// class uri
 export type BravoMonacoUri = monaco.Uri;
+
+// namespaces editor
 export type BravoMonacoTextModel = monaco.editor.ITextModel;
 export type BravoMonacoStandaloneCodeEditor = monaco.editor.IStandaloneCodeEditor;
 export type BravoMonacoStandaloneDiffEditor = monaco.editor.IStandaloneDiffEditor;
 export type BravoMonacoEditorConstructionOptions = monaco.editor.IStandaloneEditorConstructionOptions;
 export type BravoMonacoDiffEditorConstructionOptions = monaco.editor.IStandaloneDiffEditorConstructionOptions;
+
+// namespaces languages
+export type BravoMonacoCompletionItem = monaco.languages.CompletionItem;
