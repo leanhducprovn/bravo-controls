5b645a156ed7da76ebf8abfafaac567f728c8834
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-10-19 08:22:55 +0700

    Cập nhật dữ liệu
    Hoàn thành :+1:

--- src/app/components/bravo.test/bravo.test.ts
+++ src/app/components/bravo.test/bravo.test.ts
@@ -10,5 +10,16 @@ import { FormBuilder } from '@angular/forms';
 export class BravoTest implements OnInit {
     constructor(private http: HttpClient, private fb: FormBuilder) {}
 
+    test = {
+        theme: 'vs-dark',
+        language: 'txt',
+        automaticLayout: true,
+        value: 'test'
+    };
+
+    // editorOptions = { theme: 'vs-dark', language: 'javascript' };
+    // code: string = 'function x() {\nconsole.log("Hello world!");\n}';
+    // originalCode: string = 'function x() { // TODO }';
+
     ngOnInit(): void {}
 }
