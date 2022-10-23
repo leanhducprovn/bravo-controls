68f0b2d1c2f55de88fa92e4816db103cc581d740
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-10-23 08:28:33 +0700

    Khởi tạo BravoPictureEditorDemo

---
+++ src/app/demo/bravo.picture.editor.demo/bravo.picture.editor.demo.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'bravo-picture-editor-demo',
+    templateUrl: './bravo.picture.editor.demo.html',
+    styleUrls: ['./bravo.picture.editor.demo.scss']
+})
+export class BravoPictureEditorDemo implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
