858b7bdaa6cfa7e46e8919c1c8b2024ef0a19cf4
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-10-25 09:46:33 +0700

    Xóa call API
    Hoàn thành :+1:

--- src/app/demo/bravo.monaco.editor.base.demo/bravo.monaco.editor.base.demo.ts
+++ src/app/demo/bravo.monaco.editor.base.demo/bravo.monaco.editor.base.demo.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { WebDataSet } from 'core';
 import { BravoMonacoEditorConstructionOptions } from 'src/app/components/bravo.monaco.editor/bravo.monaco.editor.type';
 
 @Component({
@@ -178,37 +177,5 @@ export class BravoMonacoEditorBaseDemo implements OnInit {
 		`
     };
 
-    ngOnInit(): void {
-        this.loadXML();
-    }
-
-    private loadXML() {
-        const _api = './assets/data/bravo-editor/Declare.xml';
-        let _data: any;
-        this.http
-            .get(_api, {
-                headers: new HttpHeaders()
-                    .set('Content-Type', 'text/xml')
-                    .append('Access-Control-Allow-Methods', 'GET')
-                    .append('Access-Control-Allow-Origin', '*')
-                    .append(
-                        'Access-Control-Allow-Headers',
-                        'Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method'
-                    ),
-                responseType: 'text'
-            })
-            .subscribe(
-                (data) => {
-                    _data = data;
-                },
-                (error) => {
-                    console.log(error);
-                },
-                () => {
-                    let _ws = new WebDataSet();
-                    _ws.readXml(_data);
-                    console.log(_ws);
-                }
-            );
-    }
+    ngOnInit(): void {}
 }
