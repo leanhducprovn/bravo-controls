d7d8be145789f12b041fa56d73cc3800cb43b3f9
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-11-20 09:11:47 +0700

    Khởi tạo bravo calendar

---
+++ src/app/components/bravo.calendar/bravo.calendar.ts
@@ -0,0 +1,44 @@
+import { AfterViewInit, Component, ElementRef, forwardRef, OnDestroy, OnInit } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+
+import * as wjc from '@grapecity/wijmo';
+
+@Component({
+    selector: 'bravo-calendar',
+    templateUrl: './bravo.calendar.html',
+    styleUrls: ['./bravo.calendar.css', './bravo.calendar.scss'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            multi: true,
+            useExisting: forwardRef(() => BravoCalendar)
+        }
+    ]
+})
+export class BravoCalendar extends wjc.Control implements OnInit, AfterViewInit, OnDestroy {
+    constructor(private elRef: ElementRef) {
+        super(elRef.nativeElement);
+    }
+
+    onChange = (changed: any) => {};
+
+    onTouch = () => {};
+
+    writeValue(obj: any): void {
+        console.log(obj);
+    }
+
+    registerOnChange(changed: any): void {
+        this.onChange = changed;
+    }
+
+    registerOnTouched(touched: any): void {
+        this.onTouch = touched;
+    }
+
+    ngOnInit(): void {}
+
+    ngAfterViewInit(): void {}
+
+    ngOnDestroy(): void {}
+}
